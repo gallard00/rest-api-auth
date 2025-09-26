c3dd096d73937c97fb691cc4b7d4a693adb1916d
Nahuel Gallardo <nahuel.ng240@gmail.com>
2025-09-26 18:13:21 -0300

    feat: add User entity and repository

---
+++ src/main/java/com/gallardo/restapiauth/repository/IUserRepository.java
@@ -0,0 +1,10 @@
+package com.gallardo.restapiauth.repository;
+
+import com.gallardo.restapiauth.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface IUserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
