cd6a4bf0caf2260ad37bd2ba48407896d1d9e293
Nahuel Gallardo <nahuel.ng240@gmail.com>
2025-09-26 17:44:37 -0300

    chore: initialize Spring Boot project with base structure

---
+++ src/main/java/com/gallardo/restapiauth/RestApiAuthApplication.java
@@ -0,0 +1,13 @@
+package com.gallardo.restapiauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gallardo/restapiauth/RestApiAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gallardo.restapiauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
