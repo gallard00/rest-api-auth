7712a1cc813e7ca67bce6caa587d73e8d82e9489
Nahuel Gallardo <nahuel.ng240@gmail.com>
2025-09-30 20:27:40 -0300

    feat(dto): add AuthResponse DTO to return JWT token after login

--- src/main/java/com/gallardo/rest_api_auth/dto/response/AuthResponse.java
+++ src/main/java/com/gallardo/rest_api_auth/dto/response/AuthResponse.java
@@ -1,4 +1,10 @@
 package com.gallardo.rest_api_auth.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class AuthResponse {
+    private String token;
 }
