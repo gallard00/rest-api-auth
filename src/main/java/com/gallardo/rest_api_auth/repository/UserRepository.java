c96a265e448790bc264a702a3da13c1f621e4417
Nahuel Gallardo <nahuel.ng240@gmail.com>
2025-09-30 20:29:06 -0300

    feat(repository): extend UserRepository with method to find user by email

--- src/main/java/com/gallardo/rest_api_auth/repository/UserRepository.java
+++ src/main/java/com/gallardo/rest_api_auth/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.gallardo.rest_api_auth.repository;
 import com.gallardo.rest_api_auth.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByMail(String mail);
 }
