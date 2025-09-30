7b033c169908c9512e0e659d4eda538999455ede
Nahuel Gallardo <nahuel.ng240@gmail.com>
2025-09-30 20:26:34 -0300

    feat(dto): add LoginRequest and RegisterRequest DTOs for authentication flow

--- src/main/java/com/gallardo/rest_api_auth/dto/request/LoginRequest.java
+++ src/main/java/com/gallardo/rest_api_auth/dto/request/LoginRequest.java
@@ -1,4 +1,11 @@
 package com.gallardo.rest_api_auth.dto.request;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class LoginRequest {
+    private String email;
+    private String password;
 }
--- src/main/java/com/gallardo/rest_api_auth/dto/request/RegisterRequest.java
+++ src/main/java/com/gallardo/rest_api_auth/dto/request/RegisterRequest.java
@@ -1,4 +1,12 @@
 package com.gallardo.rest_api_auth.dto.request;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class RegisterRequest {
+    private String email;
+    private String password;
+    private String role; // Ej: ROLE_USER, ROLE_ADMIN
 }
